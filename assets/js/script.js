83092b877ff252b6d619c898e81d887c80a0bef6
szomoru <gergely.vig1978@gmail.com>
2021-08-18 09:30:43 +0000

    script.js: searchVolumeArray with random numbers added with test purpose. AddSearchVolume function update the dataBaseArray search volume object. newGAme function created, which activated by pushing Start Button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,156 +1,193 @@
-var dataBaseArray = 
+const dataBaseArray = 
 [ {name: 'Albert Einstein',
    profession: 'Physicist',
    dateOfBirth: '14th of March 1879',
    placeOfBirth: 'Germany, Ulm',
    info: 'https://en.wikipedia.org/wiki/Albert_Einstein',
-   pictureLocation: './assets/img/people/Albert_Einstein_640x640_tiny.jpg'},
+   pictureLocation: './assets/img/people/Albert_Einstein_640x640_tiny.jpg',
+   searchVolume: 0 },
 	
   {name: 'Robert Redford',
    profession: 'Actor',
    dateOfBirth: '18th of August 1936',
    placeOfBirth: 'U.S. Santa Monica CA',
    info: 'https://en.wikipedia.org/wiki/Robert_Redford',
-   pictureLocation: './assets/img/people/Robert_Redford_640x640_tiny.jpg'},
+   pictureLocation: './assets/img/people/Robert_Redford_640x640_tiny.jpg',
+   searchVolume: 0 },
 
   {name: 'Bruce Springsteen',
    profession: 'Musician',
    dateOfBirth: '23rd of September 1949',
    placeOfBirth: 'U.S. Long Branch NJ',
    info: 'https://en.wikipedia.org/wiki/Bruce_Springsteen',
-   pictureLocation: './assets/img/people/Bruce_Springsteen_640x640_tiny.jpg '},
+   pictureLocation: './assets/img/people/Bruce_Springsteen_640x640_tiny.jpg ',
+   searchVolume: 0 },
 
   {name: 'Ernest Hemingway',
    profession: 'Novelist',
    dateOfBirth: '21st of July 1899',
    placeOfBirth: 'U.S. Oak Park Illinois',
    info: 'https://en.wikipedia.org/wiki/Ernest_Hemingway',
-   pictureLocation: './assets/img/people/Ernest_Hemingway_640x640_tiny.jpg '},
+   pictureLocation: './assets/img/people/Ernest_Hemingway_640x640_tiny.jpg ',
+   searchVolume: 0 },
 
   {name: 'Muhammad Ali',
    profession: 'professional Boxer',
    dateOfBirth: '17th of January 1942',
    placeOfBirth: 'U.S. Louisville Kentucky',
    info: 'https://en.wikipedia.org/wiki/Muhammad_Ali',
-   pictureLocation: './assets/img/people/Muhammad_Ali_640x640_tiny.jpg '},
+   pictureLocation: './assets/img/people/Muhammad_Ali_640x640_tiny.jpg ',
+   searchVolume: 0 },
 
   {name: 'Stephen Hawking',
    profession: 'Theoretical physicist, Cosmologist',
    dateOfBirth: '8th of January 1942',
    placeOfBirth: 'England, Oxford',
    info: 'https://en.wikipedia.org/wiki/Stephen_Hawking',
-   pictureLocation: './assets/img/people/Stephen_Hawking_640x640_tiny.jpg '},
+   pictureLocation: './assets/img/people/Stephen_Hawking_640x640_tiny.jpg ',
+   searchVolume: 0 },
 
   {name: 'Freddy Mercury',
    profession: 'Musician',
    dateOfBirth: '5th of September 1946',
    placeOfBirth: 'Sultanate of Zanzibar, Stone Town',
    info: 'https://en.wikipedia.org/wiki/Freddie_Mercury',
-   pictureLocation: './assets/img/people/Freddy_Mercury_640x640_tiny.jpg '},
+   pictureLocation: './assets/img/people/Freddy_Mercury_640x640_tiny.jpg ',
+   searchVolume: 0 },
 
   {name: 'Jack Lemmon',
    profession: 'Actor',
    dateOfBirth: '8th of February 1925',
    placeOfBirth: 'U.S. Newton, MA',
    info: 'https://en.wikipedia.org/wiki/Jack_Lemmon',
-   pictureLocation: './assets/img/people/Jack_Lemmon_640x640_tiny.jpg '},
+   pictureLocation: './assets/img/people/Jack_Lemmon_640x640_tiny.jpg ',
+   searchVolume: 0 },
 
   {name: 'Walter Matthau',
    profession: 'Actor',
    dateOfBirth: '1st of October 1920',
    placeOfBirth: 'U.S. Lower East Side, NY',
    info: 'https://en.wikipedia.org/wiki/Walter_Matthau',
-   pictureLocation: './assets/img/people/Walter_Matthau_640x640_tiny.jpg '},
+   pictureLocation: './assets/img/people/Walter_Matthau_640x640_tiny.jpg ',
+   searchVolume: 0 },
 
   {name: 'Babe Ruth',
    profession: 'Baseball player',
    dateOfBirth: '6th of February 1895',
    placeOfBirth: 'U.S. Baltimore, Maryland',
    info: 'https://en.wikipedia.org/wiki/Babe_Ruth',
-   pictureLocation: './assets/img/people/Babe_Ruth_640x640_tiny.jpg '},
+   pictureLocation: './assets/img/people/Babe_Ruth_640x640_tiny.jpg ',
+   searchVolume: 0 },
 
   {name: 'Marie Curie',
    profession: 'Physicist',
    dateOfBirth: '7th of November 1867',
    placeOfBirth: 'Poland, Warsaw',
    info: 'https://en.wikipedia.org/wiki/Marie_Curie',
-   pictureLocation: './assets/img/people/Marie_Curie_640x640_tiny.jpg '},
+   pictureLocation: './assets/img/people/Marie_Curie_640x640_tiny.jpg ',
+   searchVolume: 0 },
 
   {name: 'Julia Roberts',
    profession: "Actress",
    dateOfBirth: '28th of October 1967',
    placeOfBirth: 'U.S. Smyrna, Georgia',
    info: 'https://en.wikipedia.org/wiki/Julia_Roberts',
-   pictureLocation: './assets/img/people/Julia_Roberts_640x640_tiny.jpg '},
+   pictureLocation: './assets/img/people/Julia_Roberts_640x640_tiny.jpg ',
+   searchVolume: 0 },
 
   {name: 'Amelia Earhart',
    profession: 'Aviation pioneer ',
    dateOfBirth: '24th of July 1897',
    placeOfBirth: 'U.S. Atchinson, Kansas',
    info: 'https://en.wikipedia.org/wiki/Amelia_Earhart',
-   pictureLocation: './assets/img/people/Amelia_Earhart_640x640_tiny.jpg '},
+   pictureLocation: './assets/img/people/Amelia_Earhart_640x640_tiny.jpg ',
+   searchVolume: 0 },
 
   {name: 'Madonna Louise Ciccone',
    profession: 'Musician',
    dateOfBirth: '16th of August 1958',
    placeOfBirth: 'U.S. Bay City, Michigan',
    info: 'https://en.wikipedia.org/wiki/Madonna',
-   pictureLocation: './assets/img/people/Madonna_640x640_tiny.jpg '},
+   pictureLocation: './assets/img/people/Madonna_640x640_tiny.jpg ',
+   searchVolume: 0 },
 
   {name: 'Marilyn Monroe',
    profession: 'Actress, Model, Singer',
    dateOfBirth: '1st of June 1926',
    placeOfBirth: 'U.S. Los Angeles, CA',
    info: 'https://en.wikipedia.org/wiki/Marilyn_Monroe',
-   pictureLocation: './assets/img/people/Marilyn_Monroe_640x640_tiny.jpg '},
+   pictureLocation: './assets/img/people/Marilyn_Monroe_640x640_tiny.jpg ',
+   searchVolume: 0 },
 
   {name: 'Steffi Graf',
    profession: 'Tennis player',
    dateOfBirth: '14th of June 1969',
    placeOfBirth: 'Germany, Manheim',
    info: 'https://en.wikipedia.org/wiki/Steffi_Graf',
-   pictureLocation: './assets/img/people/Steffi_Graf_640x640_tiny.jpg '},
+   pictureLocation: './assets/img/people/Steffi_Graf_640x640_tiny.jpg ',
+   searchVolume: 0 },
 
   {name: 'Barbra Streisand',
    profession: 'Actress, Singer',
    dateOfBirth: '24th of April 1942',
    placeOfBirth: 'U.S. New York City',
    info: 'https://en.wikipedia.org/wiki/Barbra_Streisand',
-   pictureLocation: './assets/img/people/Barbra_Streisand_640x640_tiny.jpg '},
+   pictureLocation: './assets/img/people/Barbra_Streisand_640x640_tiny.jpg ',
+   searchVolume: 0 },
 
   {name: 'Cher',
    profession: 'Musician',
    dateOfBirth: '20th of May 1946',
    placeOfBirth: 'U.S. El Centro, CA',
    info: 'https://en.wikipedia.org/wiki/Cher',
-   pictureLocation: './assets/img/people/Cher_640x640_tiny.jpg '},
+   pictureLocation: './assets/img/people/Cher_640x640_tiny.jpg ',
+   searchVolume: 0 },
 
   {name: 'Whoopi Goldberg',
    profession: 'Actress',
    dateOfBirth: '13th of November 1955',
    placeOfBirth: 'U.S. New York City',
    info: 'https://en.wikipedia.org/wiki/Whoopi_Goldberg',
-   pictureLocation: './assets/img/people/Whoopi_Goldberg_640x640_tiny.jpg '},
+   pictureLocation: './assets/img/people/Whoopi_Goldberg_640x640_tiny.jpg ',
+   searchVolume: 0 },
 
   {name: 'Mata Hari',
    profession: 'Exotic dancer, Spy',
    dateOfBirth: '7th of August 1876',
    placeOfBirth: 'Netherlands, Leeuwarden',
    info: 'https://en.wikipedia.org/wiki/Mata_Hari',
-   pictureLocation: './assets/img/people/Mata_Hari_640x640_tiny.jpg '}]
+   pictureLocation: './assets/img/people/Mata_Hari_640x640_tiny.jpg ',
+   searchVolume: 0 }]
 
-   
-   let i
-   let j
+   var searchVolumeArray = [25, 56, 89, 24, 65, 687, 357, 367, 687, 123, 74354, 45, 68, 72, 732, 7423, 682, 98, 426, 3247]
 
-   let compOneNumber=Math.floor(Math.random()*20);
-   let compTwoNumber=Math.floor(Math.random()*20);
 
-   if (compOneNumber == compTwoNumber) {
-      let compTwoNumber=Math.floor(Math.random()*20);
+   document.addEventListener("DOMContentLoaded", function () {
+      document.getElementById("startBtn").addEventListener("click", newGame);
+   })
+
+
+
+   function addSearchVolume() {
+      for (let i=0; i<20; i++) {
+         dataBaseArray[i]["searchVolume"] = searchVolumeArray[i];
+         console.log(dataBaseArray[i].searchVolume);
+      }
    }
 
+   
+   function newGame(){
+      let newGameArray = [];
+      /*for (let i=0; i<20; i++) {
+         dataBaseArray[i]["searchVolume"] = searchVolumeArray[i];
+      }*/
+      addSearchVolume();
+      for (let i=0; i<10; i++) {
+         let randomNumber = Math.floor(Math.random()*10);
+         console.log(randomNumber);
+         newGameArray.push(dataBaseArray[randomNumber]); 
+      }
+      console.log(newGameArray);
+   }
 
-
-   let newGameArray = []
-
+   
