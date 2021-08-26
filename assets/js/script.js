9f98012ba80c6159a3c327a2881320c6ed7ba78d
szomoru <gergely.vig1978@gmail.com>
2021-08-26 05:31:13 +0000

    database array removed from the script.js, database.js added to the reference in game.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,216 +1,59 @@
-const dataBaseArray = 
-[ {name: 'Albert Einstein',
-   profession: 'Physicist',
-   dateOfBirth: '14th of March 1879',
-   placeOfBirth: 'Germany, Ulm',
-   info: 'https://en.wikipedia.org/wiki/Albert_Einstein',
-   pictureLocation: './assets/img/people/Albert_Einstein_640x640_tiny.jpg',
-   searchVolume: 0 },
-	
-  {name: 'Robert Redford',
-   profession: 'Actor',
-   dateOfBirth: '18th of August 1936',
-   placeOfBirth: 'U.S. Santa Monica CA',
-   info: 'https://en.wikipedia.org/wiki/Robert_Redford',
-   pictureLocation: './assets/img/people/Robert_Redford_640x640_tiny.jpg',
-   searchVolume: 0 },
-
-  {name: 'Bruce Springsteen',
-   profession: 'Musician',
-   dateOfBirth: '23rd of September 1949',
-   placeOfBirth: 'U.S. Long Branch NJ',
-   info: 'https://en.wikipedia.org/wiki/Bruce_Springsteen',
-   pictureLocation: './assets/img/people/Bruce_Springsteen_640x640_tiny.jpg ',
-   searchVolume: 0 },
-
-  {name: 'Ernest Hemingway',
-   profession: 'Novelist',
-   dateOfBirth: '21st of July 1899',
-   placeOfBirth: 'U.S. Oak Park Illinois',
-   info: 'https://en.wikipedia.org/wiki/Ernest_Hemingway',
-   pictureLocation: './assets/img/people/Ernest _Hemingway_640x640_tiny.jpg ',
-   searchVolume: 0 },
-
-  {name: 'Muhammad Ali',
-   profession: 'professional Boxer',
-   dateOfBirth: '17th of January 1942',
-   placeOfBirth: 'U.S. Louisville Kentucky',
-   info: 'https://en.wikipedia.org/wiki/Muhammad_Ali',
-   pictureLocation: './assets/img/people/Muhammad_Ali_640x640_tiny.jpg ',
-   searchVolume: 0 },
-
-  {name: 'Stephen Hawking',
-   profession: 'Theoretical physicist, Cosmologist',
-   dateOfBirth: '8th of January 1942',
-   placeOfBirth: 'England, Oxford',
-   info: 'https://en.wikipedia.org/wiki/Stephen_Hawking',
-   pictureLocation: './assets/img/people/Stephen_Hawking_640x640_tiny.jpg ',
-   searchVolume: 0 },
-
-  {name: 'Freddie Mercury',
-   profession: 'Musician',
-   dateOfBirth: '5th of September 1946',
-   placeOfBirth: 'Sultanate of Zanzibar, Stone Town',
-   info: 'https://en.wikipedia.org/wiki/Freddie_Mercury',
-   pictureLocation: './assets/img/people/Freddie_Mercury_640x640_tiny.jpg ',
-   searchVolume: 0 },
-
-  {name: 'Jack Lemmon',
-   profession: 'Actor',
-   dateOfBirth: '8th of February 1925',
-   placeOfBirth: 'U.S. Newton, MA',
-   info: 'https://en.wikipedia.org/wiki/Jack_Lemmon',
-   pictureLocation: './assets/img/people/Jack_Lemmon_640x640_tiny.jpg ',
-   searchVolume: 0 },
-
-  {name: 'Walter Matthau',
-   profession: 'Actor',
-   dateOfBirth: '1st of October 1920',
-   placeOfBirth: 'U.S. Lower East Side, NY',
-   info: 'https://en.wikipedia.org/wiki/Walter_Matthau',
-   pictureLocation: './assets/img/people/Walter_Matthau_640x640_tiny.jpg ',
-   searchVolume: 0 },
-
-  {name: 'Babe Ruth',
-   profession: 'Baseball player',
-   dateOfBirth: '6th of February 1895',
-   placeOfBirth: 'U.S. Baltimore, Maryland',
-   info: 'https://en.wikipedia.org/wiki/Babe_Ruth',
-   pictureLocation: './assets/img/people/Babe_Ruth_640x640_tiny.jpg ',
-   searchVolume: 0 },
-
-  {name: 'Marie Curie',
-   profession: 'Physicist',
-   dateOfBirth: '7th of November 1867',
-   placeOfBirth: 'Poland, Warsaw',
-   info: 'https://en.wikipedia.org/wiki/Marie_Curie',
-   pictureLocation: './assets/img/people/Marie_Curie_640x640_tiny.jpg ',
-   searchVolume: 0 },
-
-  {name: 'Julia Roberts',
-   profession: "Actress",
-   dateOfBirth: '28th of October 1967',
-   placeOfBirth: 'U.S. Smyrna, Georgia',
-   info: 'https://en.wikipedia.org/wiki/Julia_Roberts',
-   pictureLocation: './assets/img/people/Julia_Roberts_640x640_tiny.jpg ',
-   searchVolume: 0 },
-
-  {name: 'Amelia Earhart',
-   profession: 'Aviation pioneer ',
-   dateOfBirth: '24th of July 1897',
-   placeOfBirth: 'U.S. Atchinson, Kansas',
-   info: 'https://en.wikipedia.org/wiki/Amelia_Earhart',
-   pictureLocation: './assets/img/people/Amelia_Earhart_640x640_tiny.jpg ',
-   searchVolume: 0 },
-
-  {name: 'Madonna Louise Ciccone',
-   profession: 'Musician',
-   dateOfBirth: '16th of August 1958',
-   placeOfBirth: 'U.S. Bay City, Michigan',
-   info: 'https://en.wikipedia.org/wiki/Madonna',
-   pictureLocation: './assets/img/people/Madonna_640x640_tiny.jpg ',
-   searchVolume: 0 },
-
-  {name: 'Marilyn Monroe',
-   profession: 'Actress, Model, Singer',
-   dateOfBirth: '1st of June 1926',
-   placeOfBirth: 'U.S. Los Angeles, CA',
-   info: 'https://en.wikipedia.org/wiki/Marilyn_Monroe',
-   pictureLocation: './assets/img/people/Marilyn_Monroe_640x640_tiny.jpg ',
-   searchVolume: 0 },
-
-  {name: 'Steffi Graf',
-   profession: 'Tennis player',
-   dateOfBirth: '14th of June 1969',
-   placeOfBirth: 'Germany, Manheim',
-   info: 'https://en.wikipedia.org/wiki/Steffi_Graf',
-   pictureLocation: './assets/img/people/Steffi_Graf_640x640_tiny.jpg ',
-   searchVolume: 0 },
-
-  {name: 'Barbra Streisand',
-   profession: 'Actress, Singer',
-   dateOfBirth: '24th of April 1942',
-   placeOfBirth: 'U.S. New York City',
-   info: 'https://en.wikipedia.org/wiki/Barbra_Streisand',
-   pictureLocation: './assets/img/people/Barbra_Streisand_640x640_tiny.jpg ',
-   searchVolume: 0 },
-
-  {name: 'Cher',
-   profession: 'Musician',
-   dateOfBirth: '20th of May 1946',
-   placeOfBirth: 'U.S. El Centro, CA',
-   info: 'https://en.wikipedia.org/wiki/Cher',
-   pictureLocation: './assets/img/people/Cher_640x640_tiny.jpg ',
-   searchVolume: 0 },
-
-  {name: 'Whoopi Goldberg',
-   profession: 'Actress',
-   dateOfBirth: '13th of November 1955',
-   placeOfBirth: 'U.S. New York City',
-   info: 'https://en.wikipedia.org/wiki/Whoopi_Goldberg',
-   pictureLocation: './assets/img/people/Whoopi_Goldberg_640x640_tiny.jpg ',
-   searchVolume: 0 },
-
-  {name: 'Mata Hari',
-   profession: 'Exotic dancer, Spy',
-   dateOfBirth: '7th of August 1876',
-   placeOfBirth: 'Netherlands, Leeuwarden',
-   info: 'https://en.wikipedia.org/wiki/Mata_Hari',
-   pictureLocation: './assets/img/people/Mata_Hari_640x640_tiny.jpg ',
-   searchVolume: 0 }]
-
 var searchVolumeArray = [25, 56, 89, 24, 65, 687, 357, 367, 687, 123, 74354, 45, 68, 72, 732, 7423, 682, 98, 426, 3247]
-
 var newGameArray = [];
-
-
-
 var winnerName, userAnswer;
 
-document.addEventListener("DOMContentLoaded", function () {                /* --- check if everything is loaded and listen if the Start New Game button is clicked ---*/
+document.addEventListener("DOMContentLoaded", function () {
+   /* --- check if everything is loaded and listen if the Start New Game button is clicked ---*/
    document.getElementById("startBtn").addEventListener("click", newGame);
 })
 
-function addSearchVolume() {                 /* --- Fill up the search Volume object value in the Database array ---*/
-   for (let i=0; i<20; i++) {
+function addSearchVolume() {
+   /* --- Fill up the search Volume object value in the Database array ---*/
+   for (let i = 0; i < 20; i++) {
       dataBaseArray[i]["searchVolume"] = searchVolumeArray[i];
       console.log(dataBaseArray[i].searchVolume);
    }
 }
 
-function newGame(){     /* --- This function is the main function and executed when the user clicks on the Start New Game button ---*/
+function newGame() {
+   /* --- This function is the main function and executed when the user clicks on the Start New Game button ---*/
    document.getElementById("questionNumber").innerText = 0;
    document.getElementById("correctAnswer").innerText = 0;
    document.getElementById("wrongAnswer").innerText = 0;
-   addSearchVolume();    /* --- calling addSearchVolume function ---*/
-   shuffleArray();      /* --- calling shuffleArray function ---*/
-   newGameArray = new Array;        /* --- define the array as new that will contain the comp pairs from the shuffled array. by defining as new array makes it happen that every time when the user clicks (Start game button the game array brand new) ---*/
-   
-   for (let j=0; j<arrayToShuffle.length; j+=2) {             /* --- From the shuffled array creates the game array, which contains 10 2 elements array ---*/   
-      newGameArray.push([dataBaseArray[arrayToShuffle[j]],dataBaseArray[arrayToShuffle[j+1]]]);  /* --- create the new game array from shuffled array  ---*/
+   addSearchVolume(); /* --- calling addSearchVolume function ---*/
+   shuffleArray(); /* --- calling shuffleArray function ---*/
+   newGameArray = new Array; /* --- define the array as new that will contain the comp pairs from the shuffled array. by defining as new array makes it happen that every time when the user clicks (Start game button the game array brand new) ---*/
+
+   for (let j = 0; j < arrayToShuffle.length; j += 2) {
+      /* --- From the shuffled array creates the game array, which contains 10 2 elements array ---*/
+      newGameArray.push([dataBaseArray[arrayToShuffle[j]], dataBaseArray[arrayToShuffle[j + 1]]]); /* --- create the new game array from shuffled array  ---*/
    }
    console.log(newGameArray);
    updateGameArea();
 }
 
-function shuffleArray () {                      /* --- This function randomly shuffle my basic array ---*/
-      arrayToShuffle = new Array;               /* --- define the array that will be shuffled ---*/
-      let arrayToShuffleLength = 20;
-      
-      for (let i=0; i<arrayToShuffleLength; i++){     /* --- creates the array that will be shuffled ---*/
-         arrayToShuffle[i]= i;
-      }
+function shuffleArray() {
+   /* --- This function randomly shuffle my basic array ---*/
+   arrayToShuffle = new Array; /* --- define the array that will be shuffled ---*/
+   let arrayToShuffleLength = 20;
 
-      for (let x = arrayToShuffle.length - 1; x > 0; x--) {       /* --- shuffle the created array according to the Fisher-Yates algorithm ---*/
-         let y = Math.floor(Math.random() * (x+1));         
-         const temp = arrayToShuffle[x];
-         arrayToShuffle[x] = arrayToShuffle[y];
-         arrayToShuffle[y] = temp;
-      }
-      console.log(arrayToShuffle);
+   for (let i = 0; i < arrayToShuffleLength; i++) {
+      /* --- creates the array that will be shuffled ---*/
+      arrayToShuffle[i] = i;
+   }
+
+   for (let x = arrayToShuffle.length - 1; x > 0; x--) {
+      /* --- shuffle the created array according to the Fisher-Yates algorithm ---*/
+      let y = Math.floor(Math.random() * (x + 1));
+      const temp = arrayToShuffle[x];
+      arrayToShuffle[x] = arrayToShuffle[y];
+      arrayToShuffle[y] = temp;
+   }
+   console.log(arrayToShuffle);
 }
 
-function createQuestionCard(user){                          /* --- This function fetching the object values into the HTML content of the comp-section  ---*/
+function createQuestionCard(user) {
+   /* --- This function fetching the object values into the HTML content of the comp-section  ---*/
    return `
          <p><strong>Name: </strong><span id="name" name_id="${user.name}">${user.name}</span></p>
          <p><strong>Profession: </strong>${user.profession}</p>
@@ -218,92 +61,95 @@ function createQuestionCard(user){                          /* --- This function
          <p><strong>Place of birth: </strong>${user.placeOfBirth}</p>
          <p><strong>More info: </strong><a href="${user.info}"  target="_blank">Wikipedia</a></p>`
 
+}
+
+function updateGameArea() {
+   console.log("The number of the question is: " + incrementNrOfQuestion());
+   if (nrOfQuestion == 11) {
+      endGame();
+   } else {
+      /* --- This function updating the HTML elements in the comp-section---*/
+
+      let compSectionLeftImg = document.getElementById("compImgLeft");
+      compSectionLeftImg.setAttribute("src", newGameArray[nrOfQuestion - 1][0].pictureLocation);
+      compSectionLeftImg.setAttribute("alt", newGameArray[nrOfQuestion - 1][0].name);
+      let compSectionLeft = document.getElementById("infoSectLeft");
+      compSectionLeft.innerHTML = createQuestionCard(newGameArray[nrOfQuestion - 1][0]);
+
+      let compSectionRightImg = document.getElementById("compImgRight");
+      compSectionRightImg.setAttribute("src", newGameArray[nrOfQuestion - 1][1].pictureLocation);
+      compSectionRightImg.setAttribute("alt", newGameArray[nrOfQuestion - 1][1].name);
+      let compSectionRight = document.getElementById("infoSectRight");
+      compSectionRight.innerHTML = createQuestionCard(newGameArray[nrOfQuestion - 1][1]);
+
+      calculateCorrectAnswer();
+      console.log("The calculated correct answer: " + calculateCorrectAnswer());
    }
 
-function updateGameArea(){ 
-   console.log("The number of the question is: " + incrementNrOfQuestion());    
-   if (nrOfQuestion == 11) { 
-      endGame();
-   } else {                                  /* --- This function updating the HTML elements in the comp-section---*/
-
-         let compSectionLeftImg = document.getElementById("compImgLeft");
-         compSectionLeftImg.setAttribute("src", newGameArray[nrOfQuestion-1][0].pictureLocation);
-         compSectionLeftImg.setAttribute("alt", newGameArray[nrOfQuestion-1][0].name);
-         let compSectionLeft = document.getElementById("infoSectLeft");
-         compSectionLeft.innerHTML= createQuestionCard(newGameArray[nrOfQuestion-1][0]);
-
-         let compSectionRightImg = document.getElementById("compImgRight");
-         compSectionRightImg.setAttribute("src", newGameArray[nrOfQuestion-1][1].pictureLocation);
-         compSectionRightImg.setAttribute("alt", newGameArray[nrOfQuestion-1][1].name);
-         let compSectionRight = document.getElementById("infoSectRight");
-         compSectionRight.innerHTML=createQuestionCard(newGameArray[nrOfQuestion-1][1]);
-
-         calculateCorrectAnswer();
-         console.log("The calculated correct answer: " + calculateCorrectAnswer());
-         }
-  
 }
 
-function calculateCorrectAnswer() {                            /* --- This function comparing the values of the searchVolume object key and fetching the name object key values. By this it calculates who id the winner of the comaprisation ---*/
-   if (newGameArray[nrOfQuestion-1][0].searchVolume > newGameArray[nrOfQuestion-1][1].searchVolume) {
-      winnerName = newGameArray[nrOfQuestion-1][0].name;}
-   else {
-      winnerName = newGameArray[nrOfQuestion-1][1].name;}
+function calculateCorrectAnswer() {
+   /* --- This function comparing the values of the searchVolume object key and fetching the name object key values. By this it calculates who id the winner of the comaprisation ---*/
+   if (newGameArray[nrOfQuestion - 1][0].searchVolume > newGameArray[nrOfQuestion - 1][1].searchVolume) {
+      winnerName = newGameArray[nrOfQuestion - 1][0].name;
+   } else {
+      winnerName = newGameArray[nrOfQuestion - 1][1].name;
+   }
    return winnerName;
-   
-} 
+
+}
 
 function userAnswerLeft() {
-   userAnswer = newGameArray[nrOfQuestion-1][0].name;
+   userAnswer = newGameArray[nrOfQuestion - 1][0].name;
    console.log("The selected user answer: " + userAnswer);
    checkAnswer();
 }
 
-function userAnswerRight(){
-   userAnswer = newGameArray[nrOfQuestion-1][1].name;
+function userAnswerRight() {
+   userAnswer = newGameArray[nrOfQuestion - 1][1].name;
    console.log("The selected user answer: " + userAnswer);
    checkAnswer();
 }
 
-
-function checkAnswer() {                               
+function checkAnswer() {
    if (userAnswer == winnerName) {
-      
+
       incrementScoreCorrect();
       updateGameArea();
    } else if (userAnswer != winnerName) {
-      
+
       incrementScoreWrong();
       updateGameArea();
    }
 }
 
-  
-function incrementNrOfQuestion() {     /* --- This function increase the nrOfQuestion variable by 1 ---*/
+function incrementNrOfQuestion() {
+   /* --- This function increase the nrOfQuestion variable by 1 ---*/
    let oldQuestionNr = parseInt(document.getElementById("questionNumber").innerText);
-   nrOfQuestion = document.getElementById("questionNumber").innerText = ++oldQuestionNr;  
+   nrOfQuestion = document.getElementById("questionNumber").innerText = ++oldQuestionNr;
    return nrOfQuestion;
 }
-   
-function incrementScoreCorrect() {     /* --- When this function is called it fetches the old CORRECT answer score value and increase it by 1 ---*/
+
+function incrementScoreCorrect() {
+   /* --- When this function is called it fetches the old CORRECT answer score value and increase it by 1 ---*/
    let oldScoreCorrect = parseInt(document.getElementById("correctAnswer").innerText);
-      console.log(oldScoreCorrect);
-      document.getElementById("correctAnswer").innerText = ++oldScoreCorrect;
-}
-      
-function incrementScoreWrong() {       /* --- When this function is called it fetches the old WRONG answer score value and increase it by 1 ---*/
-   let oldScoreWrong = parseInt(document.getElementById("wrongAnswer").innerText);
-      console.log(oldScoreWrong);
-      document.getElementById("wrongAnswer").innerText = ++oldScoreWrong;
+   console.log(oldScoreCorrect);
+   document.getElementById("correctAnswer").innerText = ++oldScoreCorrect;
 }
 
-function endGame(){
+function incrementScoreWrong() {
+   /* --- When this function is called it fetches the old WRONG answer score value and increase it by 1 ---*/
+   let oldScoreWrong = parseInt(document.getElementById("wrongAnswer").innerText);
+   console.log(oldScoreWrong);
+   document.getElementById("wrongAnswer").innerText = ++oldScoreWrong;
+}
+
+function endGame() {
    if (document.getElementById("correctAnswer").innerText > document.getElementById("wrongAnswer").innerText) {
       /*alert("CONGRATULATION YOU ARE THE ABSOLUTE WINNER!!! :)");*/
       let modal = document.getElementById("modalWinner");
       modal.style.display = "block";
-   }
-   else {
+   } else {
       let modal = document.getElementById("modalLoser");
       modal.style.display = "block";
       /*alert("SORRY DUDE! YOU WILL HAVE BETTER LUCK NEXT TIME! :(");*/
@@ -311,7 +157,7 @@ function endGame(){
 }
 
 function closeModal(event) {
-        modal.style.display = "none";
+   modal.style.display = "none";
 }
 
 
