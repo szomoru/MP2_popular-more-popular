f605169d15557a8f88829a8cde6d2497ff6e5b87
szomoru <gergely.vig1978@gmail.com>
2021-09-01 15:22:23 +0000

    reset function adde to the sendEmail script. Reset function empty the form after the message submitted

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -2,15 +2,8 @@ function sendMail(contactForm) {
     emailjs.send("CI_project", "pmpmp2", {
         "from_name": contactForm.name.value,
         "message": contactForm.message.value,
-        "from_email": contactForm.emailaddress.value
-})
-.then(
-    function(response) {
-        console.log("SUCCESS", response);
-    },
-    function(error) {
-        console.log("FAILED", error);
-    }
-);
-return false;  // To block from loading a new page
+        "from_email": contactForm.emailaddress.value});
+
+    document.getElementById("mailform").reset();
+    return false;  // To block from loading a new page
 }
